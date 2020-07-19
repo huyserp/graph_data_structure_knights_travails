0ecf7245a1a2601aa0d7ed549320a4d01b4996ac
Peter Huyser <huyserp@gmail.com>
2020-07-19 14:39:58 +0200

    creating tree, need base to stop recursion, stack level too deep

--- knight_moves.rb
+++ knight_moves.rb
@@ -1,79 +1,101 @@
-class Knight
-    attr_accessor :root, :destination
+require 'pry'
 
-    def initialize(start_position = [0, 0])
-        @root = start_position
-        @destination = []
-        @moves = [
-            move_forward_long_right, move_forward_long_left, move_forward_short_right, move_forward_short_left,
-            move_back_long_right, move_back_long_left, move_back_short_right, move_back_short_left
-        ]
+class Square #node
+    attr_accessor :data, :children, :parent
+    
+    @@square_count = 0
+    def initialize(board_position)
+        @data = board_position
+        @parent = nil
+        @children = []
+        @@square_count += 1
     end
 
-    def adjacency_list(root = @root)
-        list = []
+    def self.show_number_of_squares
+        @@square_count
+    end
+end
 
-        @moves.each do |move|
-            unless move[0] < 0 || move[0] > 7 || move[1] < 0 || move[1] > 7
-                list << Square.new(move)
+class Knight #tree
+    attr_accessor :adjacency_list, :root
+
+    def initialize(starting_position = [0, 0], destination)
+        @root = Square.new(starting_position)
+        @destination = destination
+        @adjacency_list = []
+    end
+
+    ### FIND THE BASE TO STOP THE RECURSION AND FINISH THE TREE ###
+    def create_moves_tree(root = @root)
+        root.children = move_options(root)
+        @adjacency_list << root
+        @adjacency_list << root.children
+        root.children.each do |child|
+            child.parent = root
+            elsif child.data == @destination
+                return
+            else
+                create_moves_tree(child)
             end
         end
     end
 
-    def move_forward_long_right(root = @root)
-        root = [root[0] + 2, root[1] + 1]
+    def move_options(root)
+        moves = [
+            move_forward_long_right(root), move_forward_long_left(root), move_forward_short_right(root), move_forward_short_left(root),
+            move_back_long_right(root), move_back_long_left(root), move_back_short_right(root), move_back_short_left(root)
+        ]
+        list = []
+        moves.each do |move|
+            list << move
+        end
+        list.select! { |position| (0..7).include?(position[0]) && (0..7).include?(position[1]) }
+        list.map! { |element| Square.new(element) }
+        return list
+    end
+
+    def move_forward_long_right(root)
+        square = [root.data[0] + 2, root.data[1] + 1]
     end
     
-    def move_forward_long_left(root = @root)
-        root = [root[0] + 2, root[1] - 1]
+    def move_forward_long_left(root)
+        square = [root.data[0] + 2, root.data[1] - 1]
     end
 
-    def move_forward_short_right(root = @root)
-        root = [root[0] + 1, root[1] + 2]
+    def move_forward_short_right(root)
+        square = [root.data[0] + 1, root.data[1] + 2]
     end
 
-    def move_forward_short_left(root = @root)
-        root = [root[0] + 1, root[1] - 2]
+    def move_forward_short_left(root)
+        square = [root.data[0] + 1, root.data[1] - 2]
     end
 
-    def move_back_long_right(root = @root)
-        root = [root[0] - 2, root[1] + 1]
+    def move_back_long_right(root)
+        square = [root.data[0] - 2, root.data[1] + 1]
     end
 
-    def move_back_long_left(root = @root)
-        root = [root[0] - 2, root[1] - 1]
+    def move_back_long_left(root)
+        square = [root.data[0] - 2, root.data[1] - 1]
     end
 
-    def move_back_short_right(root = @root)
-        root = [root[0] - 1, root[1] + 2]
+    def move_back_short_right(root)
+        square = [root.data[0] - 1, root.data[1] + 2]
     end 
 
-    def move_back_short_left(root = @root)
-        root = [root[0] - 1, root[1] - 2]
+    def move_back_short_left(root)
+        square = [root.data[0] - 1, root.data[1] - 2]
     end
 end
 
-class Square #node
-    attr_accessor :data
-    
-    def initialize(board_position)
-        @data = board_position
-    end
-end
+# class Board #tree
+#     attr_accessor :root
 
-# class Board
-#     def initalize
-#         @board = Array.new(8, Array.new(8, ""))
-#         self.populate_board
+#     def initalize(start)
+#         @root = build_board
 #     end
 
-#     def populate_board
-#         @board.map! do |line|
-#             row = 0
-
-
-#     end
 # end
 
-
+# k = Knight.new
+# k.knight_moves([3,3], [4,3])
 
